2f318b2bbc48febc24e0ff2b6244153b58f9bf52
SunnyChahal <chahalsunmeet@yahoo.com>
2018-10-20 15:21:39 -0700

    new

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
 #This is the main
+#Created on Oct 10, 2018
+#This is the main
+import sys
+
+#Enter the season (Summer, Winter, Fall, Spring)
+print sys.argv[1]
+
+#Enter the Location
+    #Northern hemisphere bright as NHB
+    #Northern hemisphere dark as NHD
+    #Southern hemisphere bright as SHB
+    #Southern hemisphere dark as SHD
+print sys.argv[2]
