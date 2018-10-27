520cbdae18048cca10158a901787a6f149c36d46
Mariano <Mariano.roldan12@gmail.com>
2018-10-27 14:49:49 -0700

    Finished scratch.py

--- scratch.py
+++ scratch.py
@@ -2,6 +2,7 @@ import random
 
 def scratch(maxTime,passes):
     file= open("random.txt","w")
+    timePasses = []
 
     for s in range(0,passes):
         randTime = random.randint(0,maxTime-1)
@@ -11,9 +12,11 @@ def scratch(maxTime,passes):
 
             if (i == randTime):
                 time = 1
+                timePasses.append(randTime+(maxTime*s)+1)
             else:
                 time = 0
 
             file.write("%d %lf\n" %(time,rand))
+    return timePasses
 
-scratch(90,5)
+print(scratch(5,3))
