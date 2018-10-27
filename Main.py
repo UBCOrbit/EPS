ede8e6e060bf24363c293eff8ab19efbd19938c9
Mariano <Mariano.roldan12@gmail.com>
2018-10-27 13:15:12 -0700

    Main 1.1

--- Main.py
+++ Main.py
@@ -4,11 +4,22 @@
 import sys
 
 #Enter the season (Summer, Winter, Fall, Spring)
-print sys.argv[1]
+#print sys.argv[1]
 
 #Enter the Location
     #Northern hemisphere bright as NHB
     #Northern hemisphere dark as NHD
     #Southern hemisphere bright as SHB
     #Southern hemisphere dark as SHD
-print sys.argv[2]
+#print sys.argv[2]
+
+def printToFile(Time, CDHPow, PayloadPow, COMMSPow, ADCSPow):
+    global file
+    if Time == 1:
+        file = open("Output.txt","w")
+        file.write("Time \t\t CDH \t Payload \t COMMS \t ADCS\n")
+
+    file.write(" %3d  %6d %6d %6d  %6d \n" %(Time,CDHPow ,PayloadPow,COMMSPow, ADCSPow))
+    #file.write(Time CDHPow Payload COMMSPow, ADCSPow)
+printToFile(1,3,5,6,8)
+printToFile(2,52,36,95,3)
---
+++ scratch.py
@@ -0,0 +1,19 @@
+import random
+
+def scratch(maxTime,passes):
+    file= open("random.txt","w")
+
+    for s in range(0,passes):
+        randTime = random.randint(0,maxTime-1)
+
+        for i in range(0,maxTime):
+            rand = random.uniform(0,9)
+
+            if (i == randTime):
+                time = 1
+            else:
+                time = 0
+
+            file.write("%d %lf\n" %(time,rand))
+
+scratch(90,5)
