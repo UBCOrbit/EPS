ea78f4d87cae52238294ffb4c2efe417a3a212fe
Ben Sommerfeld <32988623+Sommerfben@users.noreply.github.com>
2018-11-17 19:13:12 -0800

    Added some sanity checks to the inputs to the simulation
     Added sanity checks for:
    Unmatched number of transmits and photos
    Transmits taking place after the orbit has completed
    Transmits happening before the photo is taken
    Transmits happening before CDH has finished downloading the image

--- MultipleOrbits.py
+++ MultipleOrbits.py
@@ -1,6 +1,6 @@
 #Variables named something_time denote the time at which the action is started
 #Variables named something_duration denote the duration of the action
-#Eg. TAKE_PHOTO_TIME = 150 means to take a photo 150 seconds into the simulation and DETUMBLE_DURATION = 20 means that it takes 20 seconds to detumble
+#Eg. TAKE_PHOTO_TIMES = 150 means to take a photo 150 seconds into the simulation and DETUMBLE_DURATION = 20 means that it takes 20 seconds to detumble
 
 import Payload_Constants as PAYLOAD
 import Flags
@@ -9,10 +9,10 @@ import CDH_Modes as CDH
 import ADCS_Power_sequence as ADCS
 
 
-SIMULATION_DURATION = 86000      #One full orbit is 5400 seconds
-TAKE_PHOTO_TIME = [900,6300,11700]       #Takes a photo at simTime == TAKE_PHOTO_TIME seconds (eg 400 seconds into simulation)
+SIMULATION_DURATION =12500    #One full orbit is 5400 seconds
+TAKE_PHOTO_TIMES = [900,6300,11700]       #Takes a photo at simTime == TAKE_PHOTO_TIMES seconds (eg 400 seconds into simulation)
 TOTAL_POWER_CONSUMED = 0
-TRANSMIT_TIME = [1300,6700,12100]     #Time at which the photo is downlinked to earth. Will default to ASAP if this time is set to something before the photo is taken (eg set transmit time to 200 and take photo time to 600, it will transmit as soon as CDH has the photo data)
+TRANSMIT_TIMES = [1300,6700,12100]     #Time at which the photo is downlinked to earth. Will default to ASAP if this time is set to something before the photo is taken (eg set transmit time to 200 and take photo time to 600, it will transmit as soon as CDH has the photo data)
 SLEEP_OUT_TIME = 10     #Time at which the Cubesat is awoken from orbital deployment state
 PAYLOAD_BOOT_TIMES = []
 #Calculates the total power consumed per second
@@ -24,12 +24,12 @@ def main():
     COMMS_CONSUMPTION=[]
     PAYLOAD_CONSUMPTION=[]
     ADCS_CONSUMPTION=[]
-
+    inputSanityCheck()
+    transmission()    #Sets the values that a transmission from ground control would set
     #Simulates mission time by incrementing a variable every loop, where one loop is one mission second
     with open("PowerConsumption.txt", "w") as text_file:
         print("    COMMS     CDH     PAYLOAD     ADCS",file=text_file)
         for simTime in range(SIMULATION_DURATION):              #One Orbit
-            transmission(simTime)    #Sets the values that a transmission from ground control would set
             ADCS_power = ADCS.ADCS_power(simTime)       #Calls the ADCS power function and stores the vaule in a variable for printing later
             ADCS_CONSUMPTION.append(ADCS_power)     #Stores the variable value in the ADCS list as a new element
             COMMS_power = COMMS.commsPower(simTime)     #Repeats the above steps  for each subsystem
@@ -40,26 +40,43 @@ def main():
             PAYLOAD_CONSUMPTION.append(PAYLOAD_power)
             print("%d    %d          %d        %d             %d" % (simTime,COMMS_power,CDH_power,PAYLOAD_power,ADCS_power),file=text_file)       #Prints the values for that second in columns in units of millijoules
 
-
-
     Total_Power = sum(CDH_CONSUMPTION) + sum(COMMS_CONSUMPTION) + sum(PAYLOAD_CONSUMPTION) + sum(ADCS_CONSUMPTION) #Sums all elements of every list to compute the total power
 
     print("Total Power(J) =", Total_Power /1000)             #Total Power, divided by 1000 to change the units to Joules from millijoules
 
 #Sets the values that a transmisson from ground control would
 #Flags file is used to share information between the different python source files (Subsystems, etc)
-def transmission(simTime):
-    global SLEEP_OUT_TIME, TAKE_PHOTO_TIME, TRANSMIT_TIME, PAYLOAD_BOOT_TIMES
+def transmission():
+    global SLEEP_OUT_TIME, TAKE_PHOTO_TIMES, TRANSMIT_TIMES, PAYLOAD_BOOT_TIMES
 
-    if simTime == 0:        #Generates a list of time values that payload should boot at based on the times to take photos at
-        for times in TAKE_PHOTO_TIME:
-            boot_time = times - PAYLOAD.TURN_ON_DURATION - PAYLOAD.IDLE_UNTIL_PHOTO_DURATION -1
-            PAYLOAD_BOOT_TIMES.append(boot_time)
+    #if simTime == 0:        #Generates a list of time values that payload should boot at based on the times to take photos at
+    for times in TAKE_PHOTO_TIMES:
+        boot_time = times - PAYLOAD.TURN_ON_DURATION - PAYLOAD.IDLE_UNTIL_PHOTO_DURATION -1
+        PAYLOAD_BOOT_TIMES.append(boot_time)
 
-    Flags.TAKE_PHOTO_TIME = TAKE_PHOTO_TIME
+    Flags.TAKE_PHOTO_TIMES = TAKE_PHOTO_TIMES
     Flags.PAYLOAD_BOOT_TIMES =  PAYLOAD_BOOT_TIMES
-    Flags.TRANSMIT_TIMES = TRANSMIT_TIME
+    Flags.TRANSMIT_TIMES = TRANSMIT_TIMES
     Flags.SLEEP_OUT_TIME = SLEEP_OUT_TIME
+
+
+def inputSanityCheck():
+    global SLEEP_OUT_TIME, TAKE_PHOTO_TIMES, TRANSMIT_TIMES, PAYLOAD_BOOT_TIMES
+
+    orbit_number = 0
+    if len(TAKE_PHOTO_TIMES) != len(TRANSMIT_TIMES):
+        print("Number of photos and transmits are not equal")
+
+    for TAKE_PHOTO_TIME, TRANSMIT_TIME in zip(TAKE_PHOTO_TIMES,TRANSMIT_TIMES):
+        orbit_number = orbit_number + 1
+        if TRANSMIT_TIME - TAKE_PHOTO_TIME > 5400:
+            print("Transmit Time for Photo Time %d is after that orbit completes" % orbit_number)
+        if TRANSMIT_TIME - TAKE_PHOTO_TIME < 0:
+            print("Transmit Time %d is before the photo is taken" %(orbit_number))
+        if TRANSMIT_TIME - TAKE_PHOTO_TIME < PAYLOAD.PHOTO_TO_CDH_DURATION_MAXIMUM:
+            print("Transmit Time %d is before the photo has been transfered to CDH" %(orbit_number))
+
+
 
 
 main()
